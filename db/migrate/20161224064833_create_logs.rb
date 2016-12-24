3767ddd0ebbe43d58648c219779e4603af41a4f7
ichiren1 <ichiren1@gmail.com>
2016-12-24 07:01:36 +0000

    fix typo

--- db/migrate/20161224064833_create_logs.rb
+++ db/migrate/20161224064833_create_logs.rb
@@ -2,7 +2,7 @@ class CreateLogs < ActiveRecord::Migration[5.0]
   def change
     create_table :logs do |t|
       t.string :type
-      t.string :elementt
+      t.string :element
       t.string :result
 
       t.timestamps
